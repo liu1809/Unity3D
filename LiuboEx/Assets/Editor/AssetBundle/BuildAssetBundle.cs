ff93cd23796e7714564f3ea60d130c9e2da8f047
liu1809 <liu1809a@gmail.com>
2014-07-21 09:57:18 +0800

    1
    1

--- LiuboEx/Assets/Editor/AssetBundle/BuildAssetBundle.cs
+++ LiuboEx/Assets/Editor/AssetBundle/BuildAssetBundle.cs
@@ -3,26 +3,63 @@ using UnityEngine;
 
 public class BuildAssetBundle
 {
-    [MenuItem("LiuboEx/Build AssetBundle OneByOne Windows")]
-    static void CreateAssetBunldesOneByOneWindows()
+    [MenuItem("LiuboEx/Build AssetBundle/Win32/Build Scenes")]
+    static void CreateAssetBunldesScenes_Windows()
     {
         Caching.CleanCache();
-        //获取在Project视图中选择的所有游戏对象
-        Object[] SelectedAsset = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
-        //string[] selectPaths = AssetDatabase.GetAssetPath(Selection.gameObjects)
-        //遍历所有的游戏对象
-        foreach (Object obj in SelectedAsset)
+        
+        Object[] selectedAsset = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+
+        string[] selectPaths = new string[selectedAsset.Length];
+
+        string sceneNames = "";
+
+        for (int i = 0; i < selectedAsset.Length; i++)
         {
-            string sourcePath = AssetDatabase.GetAssetPath(obj);
-            //本地测试：建议最后将Assetbundle放在StreamingAssets文件夹下，如果没有就创建一个，因为移动平台下只能读取这个路径
-            //StreamingAssets是只读路径，不能写入
-            //服务器下载：就不需要放在这里，服务器上客户端用www类进行下载。
-            //string targetPath = Application.dataPath + "/StreamingAssets/" + obj.name + ".assetbundle";
+            selectPaths[i] = AssetDatabase.GetAssetPath(selectedAsset[i]);
+            if (!selectPaths[i].Contains(".unity"))
+            {
+                Debug.LogError("打包失败！场景选的不对！");
+                return;
+            }
 
-            //string targetPath = sourcePath.Substring(0, sourcePath.IndexOf('.')) + ".assetBundle";
-            string targetPath = sourcePath.Replace('.', '_') + ".assetBundle_Windows";
+            sceneNames += selectedAsset[i].name;
 
-            if (BuildPipeline.BuildAssetBundle(obj, null, targetPath, BuildAssetBundleOptions.CollectDependencies, BuildTarget.StandaloneWindows))
+            if (i < selectedAsset.Length - 1)
+            {
+                sceneNames += "-";
+            }
+        }
+        string targetPath = Application.streamingAssetsPath + "/AssetBundles/Win32/Scenes/";
+
+        if(!System.IO.Directory.Exists(targetPath))
+        {
+            System.IO.Directory.CreateDirectory(targetPath);
+        }
+
+        targetPath += (sceneNames + "_Win32_Scene.assetbundle");
+
+        BuildPipeline.BuildStreamedSceneAssetBundle(selectPaths, targetPath, BuildTarget.StandaloneWindows);
+
+        Debug.Log(sceneNames + "打包成功。\n路径:" + targetPath);
+
+        AssetDatabase.Refresh();
+    }
+
+    [MenuItem("LiuboEx/Build AssetBundle/Win32/Build Object One By One")]
+    static void CreateAssetBunldesOneByOne_Windows()
+    {
+        Caching.CleanCache();
+        
+        Object[] selectedAsset = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+
+        string targetPath = Application.streamingAssetsPath +"/AssetBundles/Win32/Objects/";
+
+        foreach (Object obj in selectedAsset)
+        {
+            string targetName = obj.name.Replace('.','_') + "_Win32_Object.assetBundle";
+
+            if (BuildPipeline.BuildAssetBundle(obj, null, targetPath+targetName, BuildAssetBundleOptions.CollectDependencies, BuildTarget.StandaloneWindows))
             {
                 Debug.Log(obj.name + "资源打包成功");
             }
@@ -31,11 +68,11 @@ public class BuildAssetBundle
                 Debug.Log(obj.name + "资源打包失败");
             }
         }
-        //刷新编辑器
+        
         AssetDatabase.Refresh();
     }
 
-    [MenuItem("LiuboEx/Build AssetBundle OneByOne Android")]
+    [MenuItem("LiuboEx/Build AssetBundle/Android/OneByOne")]
     static void CreateAssetBunldesOneByOneAndroid()
     {
         Caching.CleanCache();
@@ -73,7 +110,7 @@ public class BuildAssetBundle
     {
         Caching.CleanCache();
 
-        string Path = Application.dataPath + "/StreamingAssets/ALL.assetbundle";
+        string Path = Application.streamingAssetsPath + "/ALL.assetbundle";
 
         Object[] SelectedAsset = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
 
---
+++ LiuboEx/Assets/Scripts/Test/LoadScenes.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.IO;
+public class LoadScenes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if(GUILayout.Button("asdf"))
+        {
+            StartCoroutine(IE_Load());
+        }
+    }
+
+    IEnumerator IE_Load()
+    {
+        string terrainPath = Application.streamingAssetsPath + "/BuildScenes_Scene.assetbundle";
+        if (System.IO.File.Exists(terrainPath))
+        {
+            byte[] buffer;
+
+            using (FileStream stream = new FileStream(terrainPath, FileMode.Open, FileAccess.Read))
+            {
+                buffer = new byte[stream.Length];
+
+                stream.Read(buffer, 0, (int)stream.Length);
+            }
+
+            AssetBundleCreateRequest assetRequest = AssetBundle.CreateFromMemory(buffer);
+
+            yield return assetRequest;
+
+            AssetBundle assetBundle = assetRequest.assetBundle;
+
+            Application.LoadLevel("BuildScenes");
+
+            //if (assetRequest.isDone)
+            //{
+            //    terrainGO = Instantiate(assetBundle.mainAsset) as GameObject;
+
+            //    terrainGO.transform.parent = transform;
+            //}
+
+
+        }
+    }
+}
